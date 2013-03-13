992fa9b1885bd3e94f05557441a3cc4bdd5a8b8f
Apprentice <apprentice@devbootcamp.com>
2013-03-13 11:41:51 -0700

    completed core functionality

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,36 @@
+# TODO implement "redirects" model
+
+before do 
+  @urls = Url.order("clicks DESC")
+end
+
 get '/' do
-  # Look in app/views/index.erb
+  # let user create new short URL, display a list of shortened URLs
+  redirect ('/urls')
+end
+
+get '/urls' do
   erb :index
+end
+
+post '/urls' do
+  @url = Url.create(long_url: params[:long_url])
+  if @url.valid?
+    redirect ('/')
+  else
+    erb :index
+  end
+end
+
+# e.g., /q6bda
+get '/:short_url' do
+  url_obj = Url.where('short_url = ?', params[:short_url])[0]
+  url_obj.clicks +=1
+  url_obj.save  
+  if url_obj.long_url =~ /http/
+    redirect "http://" + url_obj.long_url.gsub(/http:\/\//, "")
+  else
+    redirect "http://" + url_obj.long_url
+  end
+
 end
---
+++ app/models/url.rb
@@ -0,0 +1,13 @@
+class Url < ActiveRecord::Base
+  before_create :create_short_url
+  VALID_URL_REGEX = /(http:)?(www\.)?.*\.\w{2,5}($|\/.*)/
+  validates :long_url, format: { with: VALID_URL_REGEX }
+  # attr_accessor :long_url, :short_url
+  #TODO validate uniqueness of short url before saving
+  # validates :short_url, uniqueness: true
+  
+  def create_short_url
+    rand_url = ('a'..'z').to_a.shuffle[0..5].join
+    self.short_url = "#{rand_url}"
+  end
+end
