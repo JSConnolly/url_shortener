e8b070e3ae906029a3ef6c0a12706d57b3f4c12b
J Connolly <jsconnolly1@gmail.com>
2013-03-13 21:43:31 -0700

    bootstrap installed, no styling applied yet

---
+++ public/js/application.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  // $('form').submit(function(e){
+  //   e.preventDefault();
+
+  //   var data = $(this).serialize();
+  //   $.ajax({
+  //     type: "POST",
+  //     url: '/post/submit',//data.url,
+  //     data: data
+  //   }).done(function(response){
+  //     $('#anagrams').text(response)});
+  // });
+
+});
